953887dbb5124031f1b3582d8a33f074811100ed
Philipp Köhn <kueken2001@gmail.com>
2023-12-28 11:35:13 +0100

    added float. point decode function (#00015)

--- headers/decode/decode.c
+++ headers/decode/decode.c
@@ -1,4 +1,5 @@
 #include "bits.h"
+#include "encode.h"
 #include "decode.h"
 #include <math.h>
 #include <stdio.h>
@@ -36,10 +37,16 @@ double decode_exzess_q(char *encoding, int q)
 
 double decode_fixed_point(char *encoding, int after_coma)
 {
-        int digits = (int)strlen(encoding);
+        int digits, i;
         int power = 0;
         double decode = 0;
-
+        digits = 0;
+        i = 0;
+        while (*(encoding + i) == '0' || *(encoding + i) == '1')
+        {
+                ++digits;
+                ++i;
+        }
         while (digits != 0)
         {
                 decode = decode + (int)(*(encoding + digits - 1) - 48) * pow(2, power);
@@ -49,4 +56,142 @@ double decode_fixed_point(char *encoding, int after_coma)
 
         decode = decode / pow(2, after_coma);
         return decode;
+}
+double decode_floating_point(char *encoding, int mantissa_pre_sign, int bits)
+{
+        char *mantissa;
+        char *exponent;
+        double decode;
+        double decoded_exponent;
+        double decoded_mantissa;
+        int i;
+
+        if (bits == 8 && mantissa_pre_sign == 5)
+        {
+                mantissa = malloc(4 * sizeof(char));
+                exponent = malloc(3 * sizeof(char));
+                if (mantissa == NULL || exponent == NULL)
+                {
+                        ALLOCATION_ERROR_MESSAGE();
+                        return ALLOCATION_ERROR;
+                }
+                for (i = 0; i < 4; ++i)
+                {
+                        *(mantissa + 3 - i) = *(encoding + 7 - i);
+                }
+                printf("\nDie codierte  Mantisse: ");
+                for (i = 0; i < 4; ++i)
+                {
+                        printf("%c", *(mantissa + i));
+                }
+                printf("\n");
+                for (i = 0; i < 3; ++i)
+                {
+                        *(exponent + 2 - i) = *(encoding + 3 - i);
+                }
+                decoded_mantissa = decode_fixed_point(mantissa, 4);
+                decoded_exponent = decode_exzess_q(exponent, pow(2, bits - mantissa_pre_sign - 1) - 1);
+                decoded_mantissa = decoded_mantissa + 1;
+                decode = decoded_mantissa * pow(2, decoded_exponent);
+
+                printf("\nDas Vorzeichenbit: %c\n", *encoding);
+                printf("\nDer Exponent: %f\n", decoded_exponent);
+                printf("\nDie Mantisse: %f\n", decoded_mantissa);
+
+                if (*(encoding) == '1')
+                {
+                        decode = decode * -1;
+                }
+
+                free(mantissa);
+                free(exponent);
+                return decode;
+        }
+        else if (bits == 16 && mantissa_pre_sign == 11)
+        {
+                mantissa = malloc(10 * sizeof(char));
+                exponent = malloc(5 * sizeof(char));
+                if (mantissa == NULL || exponent == NULL)
+                {
+                        ALLOCATION_ERROR_MESSAGE();
+                        return ALLOCATION_ERROR;
+                }
+                for (i = 0; i < 10; ++i)
+                {
+                        *(mantissa + 9 - i) = *(encoding + 15 - i);
+                }
+                printf("\nDie codierte  Mantisse: ");
+                for (i = 0; i < 10; ++i)
+                {
+                        printf("%c", *(mantissa + i));
+                }
+                printf("\n");
+                for (i = 0; i < 5; ++i)
+                {
+                        *(exponent + 4 - i) = *(encoding + 5 - i);
+                }
+                decoded_mantissa = decode_fixed_point(mantissa, 10);
+                decoded_exponent = decode_exzess_q(exponent, pow(2, bits - mantissa_pre_sign - 1) - 1);
+                decoded_mantissa = decoded_mantissa + 1;
+                decode = decoded_mantissa * pow(2, decoded_exponent);
+
+                printf("\nDas Vorzeichenbit: %c\n", *encoding);
+                printf("\nDer Exponent: %f\n", decoded_exponent);
+                printf("\nDie Mantisse: %f\n", decoded_mantissa);
+
+                if (*(encoding) == '1')
+                {
+                        decode = decode * -1;
+                }
+
+                free(mantissa);
+                free(exponent);
+                return decode;
+        }
+        else if (bits == 32 && mantissa_pre_sign == 24)
+        {
+                mantissa = malloc(23 * sizeof(char));
+                exponent = malloc(8 * sizeof(char));
+                if (mantissa == NULL || exponent == NULL)
+                {
+                        ALLOCATION_ERROR_MESSAGE();
+                        return ALLOCATION_ERROR;
+                }
+                for (i = 0; i < 23; ++i)
+                {
+                        *(mantissa + 22 - i) = *(encoding + 31 - i);
+                }
+                printf("\nDie codierte  Mantisse: ");
+                for (i = 0; i < 23; ++i)
+                {
+                        printf("%c", *(mantissa + i));
+                }
+                printf("\n");
+                for (i = 0; i < 8; ++i)
+                {
+                        *(exponent + 7 - i) = *(encoding + 8 - i);
+                }
+                decoded_mantissa = decode_fixed_point(mantissa, 23);
+                decoded_exponent = decode_exzess_q(exponent, pow(2, bits - mantissa_pre_sign - 1) - 1);
+                decoded_mantissa = decoded_mantissa + 1;
+                decode = decoded_mantissa * pow(2, decoded_exponent);
+
+                printf("\nDas Vorzeichenbit: %c\n", *encoding);
+                printf("\nDer Exponent: %f\n", decoded_exponent);
+                printf("\nDie Mantisse: %f\n", decoded_mantissa);
+
+                if (*(encoding) == '1')
+                {
+                        decode = decode * -1;
+                }
+
+                free(mantissa);
+                free(exponent);
+                return decode;
+        }
+        else
+        {
+                FALSE_VALUE_MESSAGE();
+                return FALSE_VALUE;
+        }
 }
--- headers/decode/decode.h
+++ headers/decode/decode.h
@@ -1,10 +1,11 @@
 #ifndef DECODE_H_INCLUDED
 #define DECODE_H_INCLUDED
 
-
 /*Decodes the exzess-q encoding which is written in the char pointer concerning the given q-value. Returns the decoded value.*/
 double decode_exzess_q(char *encoding, int q);
 /*Decodes the fixed point encoding which is written in the char pointer concerning the given amount of after coma digits which were used in the encoding. Returns the decoded value.*/
 double decode_fixed_point(char *encoding, int after_coma);
+/*Decodes the given floating point encoding. Amount of bits used for non charackteristics has to be set to ensure the correct decoding. Returns the decoded value. Otherwise FALSE_VALUE. Input parameters have to be identical with the used parameters for the encoding of the given encoded sequence.*/
+double decode_floating_point(char *encoding, int mantissa_pre_sign, int bits);
 
 #endif
